ac8c5ba5dd5f6846a85c358b9dd19db8b6f66edc
BHARANI17333 <133838029+BHARANI17333@users.noreply.github.com>
2023-06-07 10:37:26 +0530

    Delete cylinder.py

--- cylinder.py
+++
@@ -1,7 +0,0 @@
-p=int(input("enter the py value"))
-r=int(input("enter the r value"))
-h=int(input("enter the h value"))
-volume=p*r*r*h
-print(volume)
-surface area=((2*p*r)*h)+((p*(r**2))*2)
-print(surface area)
