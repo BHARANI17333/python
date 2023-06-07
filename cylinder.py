5723d567fb0e9d580816ca78978e391337a2722a
BHARANI17333 <133838029+BHARANI17333@users.noreply.github.com>
2023-06-07 10:37:57 +0530

    Add files via upload

---
+++ cylinder.py
@@ -0,0 +1,7 @@
+p=float(input("enter the py value"))
+r=float(input("enter the r value"))
+h=float(input("enter the h value"))
+volume=p*r*r*h
+print("volume is",volume)
+surfacearea=((2*p*r)*h)+((p*(r**2))*2)
+print("surface area is",surfacearea)
---
+++ wind chill index.py
@@ -0,0 +1,4 @@
+t=int(input("enter  t value"))
+v=int(input("enter  v value"))
+CI=(13.12 +( 0.6215*t) - (11.37*v**0.16) +( 0.3965*t*v**0.16))
+print(CI)
